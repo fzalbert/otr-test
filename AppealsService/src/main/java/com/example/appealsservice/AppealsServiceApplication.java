95ea7f9cb9a7b1d4bf314186d9fbd04291a8cc5f
Albert <fajb420@gmail.com>
2021-07-01 14:52:47 +0300

    pom fix

--- AppealsService/src/main/java/com/example/appealsservice/AppealsServiceApplication.java
+++ AppealsService/src/main/java/com/example/appealsservice/AppealsServiceApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class AppealsServiceApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(AppealsServiceApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(AppealsServiceApplication.class, args);
+    }
 
 }
