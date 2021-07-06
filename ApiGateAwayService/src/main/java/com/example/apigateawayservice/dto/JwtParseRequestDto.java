6324f22d487be4f91777d77e18064f2c15310f46
studiovr <i-790@mail.ru>
2021-07-06 15:58:28 +0300

    add

---
+++ ApiGateAwayService/src/main/java/com/example/apigateawayservice/dto/JwtParseRequestDto.java
@@ -0,0 +1,20 @@
+package com.example.apigateawayservice.dto;
+
+public class JwtParseRequestDto {
+    private String token;
+
+    public JwtParseRequestDto() {
+    }
+
+    public JwtParseRequestDto(String token) {
+        this.token = token;
+    }
+
+    public String getToken() {
+        return token;
+    }
+
+    public void setToken(String token) {
+        this.token = token;
+    }
+}
