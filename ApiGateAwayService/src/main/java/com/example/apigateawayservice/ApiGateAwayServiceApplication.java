ed40d745febd073ecb88e1eb591232e9e574f05a
Ivan <>
2021-07-08 10:18:24 +0300

    add

--- ApiGateAwayService/src/main/java/com/example/apigateawayservice/ApiGateAwayServiceApplication.java
+++ ApiGateAwayService/src/main/java/com/example/apigateawayservice/ApiGateAwayServiceApplication.java
@@ -1,7 +1,5 @@
 package com.example.apigateawayservice;
 
-import com.netflix.zuul.ZuulFilter;
-import com.netflix.zuul.context.RequestContext;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -20,34 +18,4 @@ public class ApiGateAwayServiceApplication {
         SpringApplication.run(ApiGateAwayServiceApplication.class, args);
     }
 
-    @Bean
-    public SimpleFilter simpleFilter() {
-        return new SimpleFilter();
-    }
-
-    public class SimpleFilter extends ZuulFilter {
-
-        @Override
-        public String filterType() {
-            return "pre";
-        }
-
-        @Override
-        public int filterOrder() {
-            return 1;
-        }
-
-        @Override
-        public boolean shouldFilter() {
-            return true;
-        }
-
-        @Override
-        public Object run() {
-            RequestContext ctx = RequestContext.getCurrentContext();
-            HttpServletRequest request = ctx.getRequest();
-
-            return null;
-        }
-    }
 }
