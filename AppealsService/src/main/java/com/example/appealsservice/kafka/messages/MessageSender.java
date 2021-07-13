890c1b0f701652e546c770167a7a8154596f8ee9
Меликов Иван Федорович <melikov.ivan@otr.ru>
2021-07-13 17:40:37 +0300

    add spring cloud appealsService

---
+++ AppealsService/src/main/java/com/example/appealsservice/kafka/messages/MessageSender.java
@@ -0,0 +1,33 @@
+package com.example.appealsservice.kafka.messages;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.stream.annotation.EnableBinding;
+import org.springframework.integration.support.MessageBuilder;
+import org.springframework.messaging.MessageChannel;
+import org.springframework.stereotype.Component;
+
+import javax.xml.transform.Source;
+
+@Component
+@EnableBinding(Source.class)
+public class MessageSender {
+
+    @Autowired
+    private MessageChannel output;
+
+    @Autowired
+    private ObjectMapper objectMapper;
+
+    public void send(Message<?> m) {
+        try {
+            // avoid too much magic and transform ourselves
+            String jsonMessage = objectMapper.writeValueAsString(m);
+            // wrap into a proper message for the transport (Kafka/Rabbit) and send it
+            output.send(
+                    MessageBuilder.withPayload(jsonMessage).setHeader("type", m.getType()).build());
+        } catch (Exception e) {
+            throw new RuntimeException("Could not tranform and send message due to: "+ e.getMessage(), e);
+        }
+    }
+}
