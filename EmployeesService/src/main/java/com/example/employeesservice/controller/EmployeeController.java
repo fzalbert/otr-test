8daab3bfcd60ea6b713386ff41bb116bd39555ad
Ivan <>
2021-07-16 00:56:02 +0300

    Update EmployeeController.java

--- EmployeesService/src/main/java/com/example/employeesservice/controller/EmployeeController.java
+++ EmployeesService/src/main/java/com/example/employeesservice/controller/EmployeeController.java
@@ -6,6 +6,7 @@ import com.example.employeesservice.dto.request.UpdateEmployeeDto;
 import com.example.employeesservice.dto.response.EmployeeDto;
 import com.example.employeesservice.repository.EmployeeRepository;
 import com.example.employeesservice.service.EmployeeService;
+import lombok.extern.log4j.Log4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Scope;
 import org.springframework.web.bind.annotation.*;
@@ -14,6 +15,7 @@ import javax.servlet.http.HttpServletRequest;
 import javax.validation.Valid;
 import java.util.List;
 
+@Log4j
 @RestController
 @Scope("prototype")
 @RequestMapping("employee")
@@ -30,16 +32,19 @@ public class EmployeeController extends AuthorizeController {
 
     @PostMapping("create")
     public boolean create(@Valid @RequestBody CreateEmployeeDto request) {
+        log.debug("Request method: employee/create. User login = " + request.getLogin());
         return employeeService.create(request);
     }
 
     @PostMapping("update")
     public EmployeeDto update(@RequestParam long employeeId, @RequestBody UpdateEmployeeDto request) {
+        log.debug("Request method: employee/update. User login = " + request.getLogin());
         return employeeService.update(request, employeeId);
     }
 
     @PutMapping("appoint-role")
     public boolean appointRole(@RequestParam long employeeId, RoleType role) {
+        log.debug("Request method: employee/create. UserId = " + employeeId + " role: " + role.name());
         return employeeService.appointRole(employeeId, role);
     }
 
@@ -60,13 +65,13 @@ public class EmployeeController extends AuthorizeController {
 
     @GetMapping("block")
     public void blockById(@RequestParam long id) {
-
+        log.debug("Request method: employee/block. UserId = " + id);
         this.employeeService.blockById(id);
     }
 
     @GetMapping("unblock")
     public void unblockById(@RequestParam long id) {
-
+        log.debug("Request method: employee/unblock. UserId = " + id);
         this.employeeService.unblockById(id);
     }
 }
