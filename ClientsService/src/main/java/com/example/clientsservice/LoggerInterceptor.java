e535a63ab25b2e1f887e0887848ddbacee161eaf
Меликов Иван Федорович <fajb420@gmail.com>
2021-07-17 22:17:53 +0300

    add logger

---
+++ ClientsService/src/main/java/com/example/clientsservice/LoggerInterceptor.java
@@ -0,0 +1,91 @@
+package com.example.clientsservice;
+
+import org.springframework.stereotype.Component;
+import org.springframework.web.servlet.HandlerInterceptor;
+import org.springframework.web.servlet.ModelAndView;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.util.Enumeration;
+import java.util.logging.Logger;
+import java.util.stream.Collectors;
+
+
+@Component
+public class LoggerInterceptor implements HandlerInterceptor {
+
+
+    private final Logger logger = Logger.getLogger(LoggerInterceptor.class.getName());
+
+    @Override
+    public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
+
+        logger.info("[preHandle][" + request + "]" + "[" + request.getMethod()
+                + "]" + request.getRequestURI() + getParameters(request) + "\n"
+                + makeUrl(request) + "\n"
+                + makeBody(request) + "\n"
+        );
+        return true;
+    }
+
+    @Override
+    public void postHandle(HttpServletRequest request, HttpServletResponse response, Object handler, ModelAndView modelAndView) throws Exception {
+        logger.info("[postHandle][" + request + "]");
+    }
+
+    @Override
+    public void afterCompletion(HttpServletRequest request, HttpServletResponse response, Object handler, Exception ex) throws Exception {
+        logger.info("[afterCompletion][" + request + "][exception: " + ex + "]");
+    }
+
+    private String getParameters(HttpServletRequest request) {
+        StringBuffer posted = new StringBuffer();
+        Enumeration<?> e = request.getParameterNames();
+        if (e != null) {
+            posted.append("?");
+        }
+        while (e.hasMoreElements()) {
+            if (posted.length() > 1) {
+                posted.append("&");
+            }
+            String curr = (String) e.nextElement();
+            posted.append(curr + "=");
+            if (curr.contains("password")
+                    || curr.contains("pass")
+                    || curr.contains("pwd")) {
+                posted.append("*****");
+            } else {
+                posted.append(request.getParameter(curr));
+            }
+        }
+        String ip = request.getHeader("X-FORWARDED-FOR");
+        String ipAddr = (ip == null) ? getRemoteAddr(request) : ip;
+        if (ipAddr!=null && !ipAddr.equals("")) {
+            posted.append("&_psip=" + ipAddr);
+        }
+        return posted.toString();
+    }
+
+    private String getRemoteAddr(HttpServletRequest request) {
+        String ipFromHeader = request.getHeader("X-FORWARDED-FOR");
+        if (ipFromHeader != null && ipFromHeader.length() > 0) {
+            logger.info("ip from proxy - X-FORWARDED-FOR : " + ipFromHeader);
+            return ipFromHeader;
+        }
+        return request.getRemoteAddr();
+    }
+
+    private static String makeUrl(HttpServletRequest request)
+    {
+        return request.getRequestURL().toString() + "?" + request.getQueryString();
+    }
+
+    private static String makeBody(HttpServletRequest request) throws IOException {
+        if ("POST".equalsIgnoreCase(request.getMethod()))
+        {
+            return request.getReader().lines().collect(Collectors.joining(System.lineSeparator()));
+        }
+        else return "";
+    }
+}
