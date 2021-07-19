a316632813fb26bc568489597efb677944cf3a3d
Ivan <>
2021-07-19 10:56:42 +0300

    add log in authorizes

--- AppealsService/src/main/java/com/example/appealsservice/controller/AuthorizeController.java
+++ AppealsService/src/main/java/com/example/appealsservice/controller/AuthorizeController.java
@@ -1,16 +1,24 @@
 package com.example.appealsservice.controller;
 
 import com.example.appealsservice.httpModel.UserModel;
+import lombok.extern.log4j.Log4j;
 
 import javax.servlet.http.HttpServletRequest;
 import java.net.URLDecoder;
 import java.nio.charset.StandardCharsets;
 
+@Log4j
 public abstract class AuthorizeController {
 
     protected UserModel userModel;
 
     public AuthorizeController(HttpServletRequest request) {
+
+        log.debug("Request header 'id' = " + request.getHeader("id"));
+        log.debug("Request header 'id' = " + request.getHeader("email"));
+        log.debug("Request header 'id' = " + request.getHeader("role"));
+        log.debug("Request header 'id' = " + URLDecoder.decode(request.getHeader("name"), StandardCharsets.UTF_8));
+        log.debug("Request header 'authorize' = " + request.getHeader("Authorization"));
 
         var email = request.getHeader("email");
         var name = URLDecoder.decode(request.getHeader("name"), StandardCharsets.UTF_8);
--- EmployeesService/src/main/java/com/example/employeesservice/controller/AuthorizeController.java
+++ EmployeesService/src/main/java/com/example/employeesservice/controller/AuthorizeController.java
@@ -2,6 +2,7 @@ package com.example.employeesservice.controller;
 
 import com.example.employeesservice.domain.Employee;
 import com.example.employeesservice.repository.EmployeeRepository;
+import lombok.extern.log4j.Log4j;
 
 import javax.servlet.http.HttpServletRequest;
 
@@ -9,11 +10,15 @@ import javax.servlet.http.HttpServletRequest;
 /**
  * Базовый контроллер авторизации
  */
+@Log4j
 public abstract class AuthorizeController {
 
     protected Employee employee;
 
     public AuthorizeController(HttpServletRequest request, EmployeeRepository employeeRepository) {
+
+        log.debug("Request header 'id' = " + request.getHeader("id"));
+        log.debug("Request header 'authorize' = " + request.getHeader("Authorization"));
         var employeeId = request.getHeader("id");
         if (employeeId != null)
             this.employee = employeeRepository.findById(Long.parseLong(employeeId)).orElse(null);
